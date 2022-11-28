31b2f4cbb7ea0b90611fd88f7618f887ef10c049
Brandon-Neves <84950033+Brandon-Neves@users.noreply.github.com>
2022-11-28 18:10:11 -0300

    Add files via upload

--- src/components/Chute.js
+++ src/components/Chute.js
@@ -4,7 +4,12 @@ export default function Chute(props) {
   return (
     <div className="chutar">
       <span>Já sei a palavra</span>
-      <input disabled={props.desabilitarInput} type="text" />
+      <input
+        disabled={props.desabilitarInput}
+        type="text"
+        value={props.chuteUsuario}
+        onChange={e => props.setChuteUsuario(e.target.value)}
+      />
       <button onClick={props.chutarPalavra}>Chutar</button>
     </div>
   )
